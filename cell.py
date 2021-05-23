52ed8d7b9896fbd3a7e8583600a84fe113cd67f7
vrets <vadymr@zeuselectronics.eu>
2021-05-23 20:09:08 +0300

    Initial commit

---
+++ cell.py
@@ -0,0 +1,3 @@
+class Cell:
+    def __init__(self):
+        self.info_points: int = 0
---
+++ main.py
@@ -0,0 +1,9 @@
+import asyncio
+from lattice import Lattice
+
+async def main():
+    print("Ready")
+    field = Lattice(21)
+
+if __name__ == "__main__":
+    asyncio.run(main())
